ad2cfe32f0d4b3bd69bb262baf17e3461f7b6018
Claradev32 <claraekekenta32@gmail.com>
2024-04-23 09:17:59 +0100

    few changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -12,7 +12,10 @@ const CustomSearchBox = ({ currentRefinement, refine }: any) => (
 );
 
 const SearchInput = connectSearchBox(CustomSearchBox);
-const searchClient = algoliasearch("8XVVR86Q57", "6f54d13c20586a9d3ecae50741fffa1b");
+const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '',
+  process.env.NEXT_PUBLIC_ALGOLIA_API_KEY || ''
+);
 
 const Search = () => {
   const ProductCard = ({ product }: any) => (
